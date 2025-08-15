dbfb10551c631649c2d2a319aca320a1d7cd5a09
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2025-08-15 11:42:30 +0500

    now the fox is able to look at cursor

--- Assets/Game/Scripts/LookForCursorController.cs
+++ Assets/Game/Scripts/LookForCursorController.cs
@@ -10,6 +10,11 @@ public class LookForCursorController : MonoBehaviour
     void Update()
     {
         Vector3 cursorWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition + Vector3.forward * 10);
-        transform.localPosition = cursorWorldPosition;
+        Vector3 delta = cursorWorldPosition - transform.parent.position;
+
+        float xDistance = Mathf.Lerp(0, maxXPositionDistance, delta.magnitude / maxXCursorDistance);
+        float yDistance = Mathf.Lerp(0, maxYPositionDistance, delta.magnitude / maxYCursorDistance);
+
+        transform.localPosition = new Vector3(delta.normalized.x * xDistance, delta.normalized.y * yDistance);
     }
 }
