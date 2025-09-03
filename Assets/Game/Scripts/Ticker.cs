a87f70c32c428ed9b21a3206475c1b1eed1831ca
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2025-09-03 14:51:05 +0500

    missing namespace has been restored

--- Assets/Game/Scripts/Ticker.cs
+++ Assets/Game/Scripts/Ticker.cs
@@ -1,42 +1,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ticker : MonoBehaviour
+namespace foxRestaurant
 {
-    [SerializeField] private int sliceTimeCost;
-    private List<ITickable> tickables = new List<ITickable>();
-    private TickingMode tickingMode = TickingMode.regular;
-
-    public void AddTickable(ITickable tickable)
+    public class Ticker : MonoBehaviour
     {
-        tickables.Add(tickable);
+        [SerializeField] private int sliceTimeCost;
+        private List<ITickable> tickables = new List<ITickable>();
+        private TickingMode tickingMode = TickingMode.regular;
+
+        public void AddTickable(ITickable tickable)
+        {
+            tickables.Add(tickable);
+        }
+
+        public void TickAllTickables(float tick)
+        {
+            tickables.ForEach(tickable => tickable.Tick(tick));
+        }
+
+        public void TickOnSlice() => TickAllTickables(sliceTimeCost);
+
+        private void Update()
+        {
+            if (tickingMode == TickingMode.pause)
+                return;
+
+            TickAllTickables(Time.deltaTime * (int)tickingMode);
+        }
+
+        private enum TickingMode
+        {
+            pause = 0,
+            regular = 1,
+            speedX2 = 2,
+            speedX4 = 4
+        }
+
+        public void Pause() => tickingMode = TickingMode.pause;
+        public void SetRegularTickingSpeed() => tickingMode = TickingMode.regular;
+        public void SetX2TickingSpeed() => tickingMode = TickingMode.speedX2;
+        public void SetX4TickingSpeed() => tickingMode = TickingMode.speedX4;
     }
-
-    public void TickAllTickables(float tick)
-    {
-        tickables.ForEach(tickable => tickable.Tick(tick));
-    }
-
-    public void TickOnSlice() => TickAllTickables(sliceTimeCost);
-
-    private void Update()
-    {
-        if (tickingMode == TickingMode.pause)
-            return;
-
-        TickAllTickables(Time.deltaTime * (int)tickingMode);
-    }
-
-    private enum TickingMode
-    {
-        pause = 0,
-        regular = 1,
-        speedX2 = 2,
-        speedX4 = 4
-    }
-
-    public void Pause() => tickingMode = TickingMode.pause;
-    public void SetRegularTickingSpeed() => tickingMode = TickingMode.regular;
-    public void SetX2TickingSpeed() => tickingMode = TickingMode.speedX2;
-    public void SetX4TickingSpeed() => tickingMode = TickingMode.speedX4;
 }
