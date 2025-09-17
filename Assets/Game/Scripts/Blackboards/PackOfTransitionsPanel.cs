54891ad0960f5065bfd7c345193d247a6ecec052
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2025-09-17 18:03:28 +0500

    now a transitions pack is able to display the rows of transitions (which does nothing for now)

--- Assets/Game/Scripts/Blackboards/PackOfTransitionsPanel.cs
+++ Assets/Game/Scripts/Blackboards/PackOfTransitionsPanel.cs
@@ -5,17 +5,32 @@ namespace foxRestaurant
 {
     public class PackOfTransitionsPanel : UIRepeater<RowOfTransitionsPanel, List<ItemData>>
     {
-        [SerializeField] private RowOfTransitionsPanel rowOfTransitionsPanelPrefab;
-
         public void UpdatePanels(RootData rootData)
         {
-            //UpdatePanels(rootData);
-            print(rootData.RootItemData.name);
+            List<List<ItemData>> rows = new List<List<ItemData>>();
+            var slicingIterator = rootData.RootItemData;  
+            
+            while (slicingIterator != null)
+            {
+                List<ItemData> row = new List<ItemData>();
+                ItemData fryingterator = slicingIterator;
+
+                while (fryingterator != null)
+                {
+                    row.Add(fryingterator);
+                    rootData.DataBase.FryingResults.TryGetValue(fryingterator, out fryingterator);
+                }
+
+                rows.Add(row);
+                rootData.DataBase.SlicingResults.TryGetValue(slicingIterator, out slicingIterator);
+            }
+
+            UpdatePanels(rows);
         }
 
         protected override void Bind(RowOfTransitionsPanel panel, List<ItemData> itemsData)
         {
-            panel.SetListOfItemData(itemsData);
+            panel.UpdatePanels(itemsData);
         }
     }
 }
--- Assets/Game/Scripts/Blackboards/RowOfTransitionsPanel.cs
+++ Assets/Game/Scripts/Blackboards/RowOfTransitionsPanel.cs
@@ -1,13 +1,19 @@
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 
 namespace foxRestaurant
 {
     public class RowOfTransitionsPanel : MonoBehaviour
     {
-        public void SetListOfItemData(List<ItemData> itemsData)
+        public void UpdatePanels(List<ItemData> itemsData)
         {
-
+            StringBuilder sb = new StringBuilder();
+            foreach (ItemData item in itemsData)
+            {
+                sb.Append(item.name).Append(" ");
+            }
+            print(sb.ToString());
         }
     }
 }
