9a2d5e266a2a394738d6ab4361ba7654c3faa954
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2025-09-29 15:41:01 +0500

    fade-in fade-out transitions between encounters have been added

--- Assets/Game/Scripts/Level.cs
+++ Assets/Game/Scripts/Level.cs
@@ -1,12 +1,15 @@
+using DG.Tweening;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace foxRestaurant
 {
     public class Level : MonoBehaviour
     {
         [SerializeField] private DataBetweenScenesContainer dataBetweenScenesContainer;
+        [SerializeField] private Image fading;
 
         private List<Encounter> encounters = new();
         private int currentIndex = 0;
@@ -27,6 +30,8 @@ namespace foxRestaurant
                 spawnedEncaunter.gameObject.SetActive(false);
                 spawnedEncaunter.transform.parent = transform;
             }
+
+            fading.GetComponentInParent<Canvas>().sortingOrder = 9999;
         }
 
         private async Task RunLevel()
@@ -35,10 +40,24 @@ namespace foxRestaurant
             {
                 Encounter encounter = encounters[currentIndex];
                 encounter.gameObject.SetActive(true);
+                FadeOut();
                 await encounter.StartEncounter();
+                await FadeIn();
                 encounter.gameObject.SetActive(false);
                 currentIndex++;
             }
+        }
+
+        public async Task FadeIn()
+        {
+            Tween tween = fading.DOFade(1f, 0.4f);
+            await tween.AsyncWaitForCompletion();
+        }
+
+        public async Task FadeOut()
+        {
+            Tween tween = fading.DOFade(0f, 0.4f);
+            await tween.AsyncWaitForCompletion();
         }
     }
 }
