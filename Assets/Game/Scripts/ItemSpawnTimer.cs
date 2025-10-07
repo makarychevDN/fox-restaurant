99ac89af204c25d9500820ba9ee22a1e93e058c7
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2025-10-07 19:52:55 +0500

    now a spawn timer displays info correctly

--- Assets/Game/Scripts/ItemSpawnTimer.cs
+++ Assets/Game/Scripts/ItemSpawnTimer.cs
@@ -10,9 +10,11 @@ namespace foxRestaurant
         [SerializeField] private float spawnInterval = 5f;
         private float timer;
         private TMP_Text timerDisplayer;
+        private RestaurantEncounter restaurantEncounter;
 
         public void Init(RestaurantEncounter restaurantEncounter)
         {
+            this.restaurantEncounter = restaurantEncounter;
             restaurantEncounter.Ticker.AddTickable(this);
             timerDisplayer = restaurantEncounter.SlotsManager.SpawningSlots.Last().GetComponentInChildren<TMP_Text>();
         }
@@ -22,11 +24,19 @@ namespace foxRestaurant
             timer += deltaTime;
             if (timer >= spawnInterval)
             {
+                if (restaurantEncounter.SlotsManager.SpawningSlots.Where(slot => slot.Empty).Count() == 0)
+                {
+                    timerDisplayer.text = "<mspace=1em>ERROR</mspace>";
+                    timerDisplayer.color = Extensions.HexToColor("#9c2d2d");
+                    return;
+                }
+
                 timer = 0f;
                 spawner.SpawnIngredient();
             }
 
-            timerDisplayer.text = (spawnInterval - timer).ToString();
+            timerDisplayer.text = (spawnInterval - timer).ToString("<mspace=1em>0.0s</mspace>").Replace(',', ':');
+            timerDisplayer.color = Extensions.HexToColor("#848f2e");
         }
     }
 }
