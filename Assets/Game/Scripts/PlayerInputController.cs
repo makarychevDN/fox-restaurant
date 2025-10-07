6a726466fee5e0b8826b90b05503db1281d66705
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2025-10-07 21:04:59 +0500

    spawn an ingredient button has been removed

--- Assets/Game/Scripts/PlayerInputController.cs
+++ Assets/Game/Scripts/PlayerInputController.cs
@@ -5,12 +5,10 @@ namespace foxRestaurant
 {
     public class PlayerInputController : MonoBehaviour
     {
-        [SerializeField] private Button spawnIngredientButton;
         [SerializeField] private RecipeBook recipeBook;
 
         public void Init(RestaurantEncounter restaurantEncounter)
         {
-            spawnIngredientButton.onClick.AddListener(restaurantEncounter.ItemsSpawner.SpawnIngredient);
             recipeBook.Init(restaurantEncounter);
         }
     }
