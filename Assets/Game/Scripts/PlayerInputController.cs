44cdc3d81a810c08d5d59543ec1d558fa83a897d
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2025-10-09 16:10:24 +0500

    working time controller has been added

--- Assets/Game/Scripts/PlayerInputController.cs
+++ Assets/Game/Scripts/PlayerInputController.cs
@@ -6,10 +6,18 @@ namespace foxRestaurant
     public class PlayerInputController : MonoBehaviour
     {
         [SerializeField] private RecipeBook recipeBook;
+        [SerializeField] private Button pauseButton;
+        [SerializeField] private Button playButton;
+        [SerializeField] private Button playX2Button;
+        [SerializeField] private Button playX4Button;
 
         public void Init(RestaurantEncounter restaurantEncounter)
         {
             recipeBook.Init(restaurantEncounter);
+            playButton.onClick.AddListener(restaurantEncounter.Ticker.SetRegularTickingSpeed);
+            playX2Button.onClick.AddListener(restaurantEncounter.Ticker.SetX2TickingSpeed);
+            playX4Button.onClick.AddListener(restaurantEncounter.Ticker.SetX4TickingSpeed);
+            pauseButton.onClick.AddListener(restaurantEncounter.Ticker.Pause);
         }
     }
 }
