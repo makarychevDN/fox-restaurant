6cc6cee301d663bec5ba0033e93bd81d1990d5b8
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2025-10-16 14:58:45 +0500

    a glasshour has been added

--- Assets/Game/Scripts/PlayerInputController.cs
+++ Assets/Game/Scripts/PlayerInputController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,14 +11,46 @@ namespace foxRestaurant
         [SerializeField] private Button playButton;
         [SerializeField] private Button playX2Button;
         [SerializeField] private Button playX4Button;
+        private List<Button> hourGlassButtons;
 
         public void Init(RestaurantEncounter restaurantEncounter)
         {
             recipeBook.Init(restaurantEncounter);
-            playButton.onClick.AddListener(restaurantEncounter.Ticker.SetRegularTickingSpeed);
-            playX2Button.onClick.AddListener(restaurantEncounter.Ticker.SetX2TickingSpeed);
-            playX4Button.onClick.AddListener(restaurantEncounter.Ticker.SetX4TickingSpeed);
-            pauseButton.onClick.AddListener(restaurantEncounter.Ticker.Pause);
+
+            hourGlassButtons = new List<Button>() { pauseButton, playButton, playX2Button, playX4Button };
+            pauseButton.onClick.AddListener(() => PauseButtonPressedHandler(restaurantEncounter));
+            playButton.onClick.AddListener(() => PlayButtonPressedHandler(restaurantEncounter));
+            playX2Button.onClick.AddListener(() => X2ButtonPressedHandler(restaurantEncounter));
+            playX4Button.onClick.AddListener(() => X4ButtonPressedHandler(restaurantEncounter));
+        }
+
+        private void PauseButtonPressedHandler(RestaurantEncounter restaurantEncounter)
+        {
+            restaurantEncounter.Ticker.Pause();
+            UpdateButtonsStates(pauseButton);
+        }
+
+        private void PlayButtonPressedHandler(RestaurantEncounter restaurantEncounter)
+        {
+            restaurantEncounter.Ticker.SetRegularTickingSpeed();
+            UpdateButtonsStates(playButton);
+        }
+
+        private void X2ButtonPressedHandler(RestaurantEncounter restaurantEncounter)
+        {
+            restaurantEncounter.Ticker.SetX2TickingSpeed();
+            UpdateButtonsStates(playX2Button);
+        }
+
+        private void X4ButtonPressedHandler(RestaurantEncounter restaurantEncounter)
+        {
+            restaurantEncounter.Ticker.SetX4TickingSpeed();
+            UpdateButtonsStates(playX4Button);
+        }
+
+        private void UpdateButtonsStates(Button disabledButton)
+        {
+            hourGlassButtons.ForEach(button => button.interactable = (button != disabledButton));
         }
     }
 }
