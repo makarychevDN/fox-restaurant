6fbda528e945f52f02a46d2016effbbb7f4a743c
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2025-10-21 22:12:43 +0500

    pirate restaurant environment has been added

--- Assets/Game/Scripts/Scenarios/PirateRestaurantScenarioPart1.cs
+++ Assets/Game/Scripts/Scenarios/PirateRestaurantScenarioPart1.cs
@@ -6,8 +6,10 @@ namespace foxRestaurant
     {
         public override void Init(RestaurantEncounter restaurantEncounter)
         {
-            restaurantEncounter.ItemsSpawner.SetFoodItemSpawnSlots(restaurantEncounter.SlotsManager.Slots.Where(slot => slot.RequiredItemsType == ItemType.Food && slot.gameObject.activeSelf).ToList());
+            restaurantEncounter.ItemsSpawner.SetFoodItemSpawnSlots(
+                restaurantEncounter.SlotsManager.Slots.Where(slot => slot.RequiredItemsType == ItemType.Food && slot.gameObject.activeSelf).ToList());
             print(restaurantEncounter.SlotsManager.Slots.Where(slot => slot.RequiredItemsType == ItemType.Food).ToList().Count);
+            restaurantEncounter.ItemsSpawner.SpawnIngredient();
             restaurantEncounter.ItemsSpawner.SpawnIngredient();
             print("pirate restaurant scenario part 1 inited");
         }
