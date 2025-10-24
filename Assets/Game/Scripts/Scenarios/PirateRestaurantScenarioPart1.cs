e2b5cf0d562fe5d590d8b63d3708dfa86ec66a0d
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2025-10-24 13:37:31 +0500

    the first wave works correctly

--- Assets/Game/Scripts/Scenarios/PirateRestaurantScenarioPart1.cs
+++ Assets/Game/Scripts/Scenarios/PirateRestaurantScenarioPart1.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using UnityEngine;
@@ -7,23 +8,96 @@ namespace foxRestaurant
     public class PirateRestaurantScenarioPart1 : BaseScenario<RestaurantEncounter>
     {
         [SerializeField] private ItemData iceCreamConeData;
+        [SerializeField] private ItemData popsicleData;
         [SerializeField] private ItemData iceCreamPlateData;
         [SerializeField] private ItemData cherrySyrupData;
         [SerializeField] private ItemData cherryIceCreamPlateData;
 
-        protected override async Task StartScenarioTyped(RestaurantEncounter ecnounter)
+        [SerializeField] private CustomerData doggo;
+        [SerializeField] private CustomerData kitty;
+        [SerializeField] private CustomerData duck;
+
+        [SerializeField] private List<ItemSlot> itemSlots;
+
+        [SerializeField] private Character redTheCook;
+
+        private TaskCompletionSource<bool> completionSource = new();
+        private List<Customer> customersToFeed = new();
+
+        protected override async Task StartScenarioTyped(RestaurantEncounter encounter)
         {
-            ecnounter.ItemsSpawner.SetFoodItemSpawnSlots(
-                ecnounter.SlotsManager.Slots.Where(slot => slot.RequiredItemsType == ItemType.Food && slot.gameObject.activeSelf).ToList());
-            print(ecnounter.SlotsManager.Slots.Where(slot => slot.RequiredItemsType == ItemType.Food).ToList().Count);
-            ecnounter.ItemsSpawner.SpawnIngredient();
-            ecnounter.ItemsSpawner.SpawnIngredient();
-            print("pirate restaurant scenario part 1 inited");
+            itemSlots = encounter.SlotsManager.Slots.Where(slot => slot.RequiredItemsType == ItemType.Food && slot.gameObject.activeSelf).ToList();
 
-            for (int i = 0; i < 100; i++)
-                ecnounter.CustomerSpawner.TryToSpawnCustomer(ecnounter.DecksManager.GetRandomCustomer(), () => iceCreamConeData);
+            await Task.Delay(500);
+            await redTheCook.Say("*sigh*<pause:1> Another day, another dollar");
 
-            await Task.Delay(5000);
+            await FirstWave(encounter);
+
+            await completionSource.Task;
+        }
+
+        private async Task FirstWave(RestaurantEncounter encounter)
+        {
+            bool success = false;
+            while (!success)
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    await Task.Delay(500);
+                    encounter.ItemsSpawner.SpawnFoodItem(encounter, iceCreamConeData, itemSlots[i + 1]);
+                }
+
+                for (int i = 0; i < 2; i++)
+                {
+                    await Task.Delay(500);
+
+                    var customer = encounter.CustomerSpawner.TryToSpawnCustomer(
+                        encounter.DecksManager.GetRandomCustomer(),
+                        () => iceCreamConeData
+                    );
+
+                    customersToFeed.Add(customer);
+                }
+
+                var tasks = customersToFeed.Select(WaitForCustomerToLeave).ToArray();
+                var results = await Task.WhenAll(tasks);
+
+                success = results.All(r => r);
+                if (!success)
+                    await redTheCook.Say("damn...<pause:1> ok, let's try again");
+                else
+                    await redTheCook.Say("Hell yeah!");
+            }
+        }
+
+        private Task<bool> WaitForCustomerToLeave(Customer customer)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+
+            void OnLeftHandler(bool wasSatisfied)
+            {
+                customer.OnLeft.RemoveListener(OnLeftHandler);
+                tcs.TrySetResult(wasSatisfied);
+            }
+
+            customer.OnLeft.AddListener(OnLeftHandler);
+            return tcs.Task;
+        }
+
+        public void Complete()
+        {
+            if (completionSource != null && !completionSource.Task.IsCompleted)
+            {
+                completionSource.SetResult(true);
+            }
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Complete();
+            }
         }
     }
 }
