2253193a06eba041f4ce3d4e86776f6dcb1e90af
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2026-02-18 23:30:04 +0500

    a shower scene has been finished

--- Assets/Game/Scripts/Scenarios/PirateRestaurantScenarioPart7.cs
+++ Assets/Game/Scripts/Scenarios/PirateRestaurantScenarioPart7.cs
@@ -11,25 +11,37 @@ namespace foxRestaurant
         [SerializeField] private Transform paw;
         [SerializeField] private Transform tape;
         [SerializeField] private AudioSource squekSounds;
+        [SerializeField] private ParticleSystem water;
 
         protected override void InitTyped(ListenDialoguesEncounter encounter){}
 
         protected override async Task StartScenarioTyped(ListenDialoguesEncounter encounter)
         {
+            await SceneInShower();
+        }
+
+        private async Task SceneInShower()
+        {
+            var waterMain = water.main;
+
             await paw.DOMove(new Vector3(0, -8), 0.5f).AsyncWaitForCompletion();
             squekSounds.Play();
             tape.DORotate(new Vector3(0, 0, 75), 0.5f);
             await paw.DOMove(new Vector3(0, -12.5f), 0.5f).AsyncWaitForCompletion();
+            waterMain.maxParticles = 200;
 
             await paw.DOMove(new Vector3(0, -8), 0.5f).AsyncWaitForCompletion();
-            tape.DORotate(new Vector3(0, 0, 75*2), 0.5f);
+            tape.DORotate(new Vector3(0, 0, 75 * 2), 0.5f);
             await paw.DOMove(new Vector3(0, -12.5f), 0.5f).AsyncWaitForCompletion();
+            waterMain.maxParticles = 100;
 
             await paw.DOMove(new Vector3(0, -8), 0.5f).AsyncWaitForCompletion();
-            tape.DORotate(new Vector3(0, 0, 75*3), 0.5f);
+            tape.DORotate(new Vector3(0, 0, 75 * 3), 0.5f);
             await paw.DOMove(new Vector3(0, -12.5f), 0.5f).AsyncWaitForCompletion();
+            waterMain.maxParticles = 0;
 
-            await Task.Delay(10000);
+            await Task.Delay(1500);
+            await paw.DOMove(new Vector3(0, -30), 1f).AsyncWaitForCompletion();
         }
     }
 }
