8dd68029c4af228e86b197dba6fab288b46a927f
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2026-05-18 12:09:31 +0500

    the scenario is rewriten due new current wave controller

--- Assets/Game/Scripts/Scenarios/RiversideScenarioPart4.cs
+++ Assets/Game/Scripts/Scenarios/RiversideScenarioPart4.cs
@@ -1,101 +1,50 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using UnityEngine;
+using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
 
 namespace foxRestaurant
 {
     public class RiversideScenarioPart4 : BaseScenario<RestaurantEncounter>
     {
         [SerializeField] private Character red;
+        [SerializeField] private CustomerData kitty;
+        [SerializeField] private CustomerData doggo;
         [SerializeField] private CustomerData seal;
         [SerializeField] private ItemData iceCream;
+        [SerializeField] private List<ItemSlot> slots;
+        private RestaurantEncounter encounter;
 
-        protected override void InitTyped(RestaurantEncounter encounter) { }
+        protected override void InitTyped(RestaurantEncounter encounter) 
+        {
+            this.encounter = encounter;
+        }
 
         protected override async Task StartScenarioTyped(RestaurantEncounter encounter)
         {
-            List<CustomerData> customersData = new List<CustomerData>() { seal, seal, seal };
-            await FixWave(encounter, customersData);
+            await encounter.CurrentWaveManager.ExecuteWave(
 
-            while (true)
-            {
-                await red.Say("hello");
-            }
+                new List<Func<Task>>() { 
+                    () => red.Say("до"), 
+                    () => SpawnIceCream(new List<ItemData>() { iceCream, iceCream, iceCream, iceCream} )
+                },
+
+                () => red.Say("после"),
+
+                (kitty, () => iceCream),
+                (kitty, () => iceCream));
         }
 
-        public async Task FixWave(RestaurantEncounter encounter, List<CustomerData> customersToSpawnData)
+        private async Task SpawnIceCream(List<ItemData> itemsToSpawnData)
         {
-            bool success = false;
-            int seatPlacesCount = FindObjectsByType<SeatPlace>(FindObjectsSortMode.None).ToList().Count;
-            var initCustomersCount = Mathf.Min(customersToSpawnData.Count, seatPlacesCount);
-
-            for (int i = 0; i < initCustomersCount; i++)
+            var itemSlots = encounter.SlotsManager.Slots.Where(slot => slot.RequiredItemsType == ItemType.Food && slot.gameObject.activeSelf).ToList();
+            for (int i = 1; i < itemsToSpawnData.Count; i++)
             {
                 await Task.Delay(500);
-                encounter.CustomerSpawner.TryToSpawnCustomer(customersToSpawnData[i], () => iceCream);
+                encounter.ItemsSpawner.SpawnFoodItem(encounter, itemsToSpawnData[i], itemSlots[i]);
             }
-
-            await Task.Delay(500);
         }
-
-        /*7private async Task FixWave(RestaurantEncounter encounter, List<ItemData> itemsToSpawnData, params (CustomerData, Func<ItemData>)[] customersAndTheirOrders)
-        {
-            await FixWave(encounter, itemsToSpawnData, new List<string>(), customersAndTheirOrders);
-        }
-
-        private async Task FixWave(RestaurantEncounter encounter, List<ItemData> itemsToSpawnData, string commentary, params (CustomerData, Func<ItemData>)[] customersAndTheirOrders)
-        {
-            await FixWave(encounter, itemsToSpawnData, new List<string> { commentary }, customersAndTheirOrders);
-        }
-
-        private async Task FixWave(RestaurantEncounter encounter, List<ItemData> itemsToSpawnData, List<string> commentaries, params (CustomerData, Func<ItemData>)[] customersAndTheirOrders)
-        {
-            bool success = false;
-            while (!success)
-            {
-                Time.timeScale = 1;
-                encounter.BlockInput();
-                customersToFeed.Clear();
-
-                if (itemsToSpawnData.Count > 0)
-                {
-                    itemSlots.ForEach(slot => slot.Clear());
-                    FindObjectsOfType<Item>().ToList().ForEach(item => Destroy(item.gameObject));
-                }
-
-                for (int i = 0; i < itemsToSpawnData.Count; i++)
-                {
-                    await Task.Delay(500);
-                    encounter.ItemsSpawner.SpawnFoodItem(encounter, itemsToSpawnData[i], itemSlots[i]);
-                }
-
-                foreach (var customerAndOrder in customersAndTheirOrders)
-                {
-                    await Task.Delay(500);
-
-                    var customer = encounter.CustomerSpawner.TryToSpawnCustomer(
-                        customerAndOrder.Item1,
-                        customerAndOrder.Item2
-                    );
-
-                    customersToFeed.Add(customer);
-                }
-
-                foreach (var commentary in commentaries)
-                {
-                    await red.Say(commentary);
-                }
-                encounter.UnblockInput();
-
-                var tasks = customersToFeed.Select(WaitForCustomerToLeave).ToArray();
-                var results = await Task.WhenAll(tasks);
-                Time.timeScale = 1;
-
-                success = results.All(r => r);
-                if (!success)
-                    await red.Say(waveIsFailedLine);
-            }
-        }*/
     }
 }
