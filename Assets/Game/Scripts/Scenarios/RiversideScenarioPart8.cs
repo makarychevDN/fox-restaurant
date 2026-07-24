36796326434bec28e8b8bd97c835dd4c860ade61
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2026-07-25 00:22:51 +0500

    8th scenario has been updated

--- Assets/Game/Scripts/Scenarios/RiversideScenarioPart8.cs
+++ Assets/Game/Scripts/Scenarios/RiversideScenarioPart8.cs
@@ -1,4 +1,5 @@
 using Cysharp.Threading.Tasks;
+using DG.Tweening;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -10,8 +11,16 @@ namespace foxRestaurant
     {
         [SerializeField] private Character red;
         [SerializeField] private Character adele;
+        [SerializeField] private Character boris;
+        [SerializeField] private Transform adelesEye;
+        [SerializeField] private ParticleSystem borisPoofEffect;
+        [SerializeField] private ParticleSystem adelePoofEffect;
+        [SerializeField] private List<Character> runningPeople;
+        [SerializeField] private List<ParticleSystem> runningPeoplePoofEffects;
         [SerializeField] private AudioSource successSound;
-        [SerializeField] private AudioSource impactSound;
+        [SerializeField] private AudioSource farImpactSound;
+        [SerializeField] private AudioSource poofSound;
+        [SerializeField] private AudioSource sneezeSound;
         [SerializeField] private AudioSource music;
 
         [Header("Customers")]
@@ -37,6 +46,7 @@ namespace foxRestaurant
         [SerializeField] private ItemData bigCoal;
         [SerializeField] private ItemData coal;
 
+
         [Header("SeatPlaces")]
         [SerializeField] private Table table;
 
@@ -49,8 +59,116 @@ namespace foxRestaurant
 
         protected override async UniTask StartScenarioTyped(RestaurantEncounter encounter)
         {
+            await Cutscene();
+            encounter.ItemSpawnTimer.SetBlocked(true);
+            encounter.GarbageCan.SetBlocked(true);
+            await TutorialWave();
+            encounter.ItemSpawnTimer.SetBlocked(false);
+            encounter.GarbageCan.SetBlocked(false);
+            await TheFirstWave();
+            await TheSecondWave();
+            await TheThirdWave();
+        }
+
+        private async UniTask Cutscene()
+        {
+            red.LookAt(boris.transform);
+            await UniTask.Delay(1000);
+            await BorisSneeze();
+
+            red.LookAt(runningPeople[0].transform);
+            await runningPeople[0].Say("Спасайся кто может!");
+
+            for (int i = 0; i < runningPeople.Count; i++)
+            {
+                runningPeople[i].gameObject.SetActive(false);
+                runningPeoplePoofEffects[i].Play();
+                poofSound.Play();
+                await UniTask.Delay(250);
+            }
+
+            red.LookAt(boris.transform);
+            boris.gameObject.SetActive(true);
+            borisPoofEffect.Play();
+            await BorisSneeze();
             await UniTask.Delay(1000);
 
+            await boris.Say("Охх,<pause:0.5> как здорово,<pause:0.5> у нас гости и вы устроили такой милый пикник!");
+            await boris.Say("А меня не позвали, проказники.");
+            await BorisSneeze();
+
+            adele.gameObject.SetActive(true);
+            adelePoofEffect.Play();
+            poofSound.Play();
+            await UniTask.Delay(1000);
+
+            await adele.Say("Боря, иди обратно в дом сейчас же!");
+            await adele.Say("Мы тебе принесем еды туда.");
+
+            await boris.Say("Обратно в дом?<pause:0.5> Ну уж нетушки!");
+            await boris.Say("Я не хочу пропустить все веселье с нашими новыми пушистыми друзьями!");
+
+            await BorisSneeze();
+            await UniTask.Delay(1000);
+
+            await adele.Say("Боря,<pause:0.5> умоляю тебя,<pause:0.5> ты сейчас здесь все разнесешь.");
+            await boris.Say("Не драматизируй, Адель,<pause:0.5>  не так уж сильно я и чихаю.");
+
+            await BorisSneeze();
+            await UniTask.Delay(1000);
+
+            await boris.Say("Я просто посижу с вами здесь, ребята.<pause:0.5> Я никого не побеспокою!");
+            await boris.Say("К нам так редко заходят гости!<pause:0.5> Я ни за что не упущу такую возможность!");
+
+            await UniTask.Delay(1000);
+            red.LookAt(adelesEye);
+            await adele.Say("Слушай сюда, Рыжий.");
+            await adele.Say("Делай все что угодно,<pause:0.5> но не давай ему чихнуть.");
+            await adele.Say("Если он разойдется, мы это место будем собирать по щепкам!");
+            await red.Say("Я не подведу.");
+            await red.Say("Клятва гиппопотама.");
+
+            adele.gameObject.SetActive(false);
+            adelePoofEffect.Play();
+            poofSound.Play();
+            await UniTask.Delay(500);
+
+            red.LookAt(null);
+
+            boris.gameObject.SetActive(false);
+            borisPoofEffect.Play();
+            poofSound.Play();
+            await UniTask.Delay(500);
+        }
+
+        private async UniTask BorisSneeze()
+        {
+            farImpactSound.Play();
+            sneezeSound.Play();
+            Camera.main.ShakeCamera(1);
+            await boris.transform.DOScale(1.3f, 0.1f);
+            await boris.transform.DOScale(1, 0.1f);
+            await UniTask.Delay(800);
+        }
+
+        private async UniTask TutorialWave()
+        {
+            await encounter.CurrentWaveManager.DoWaveTillComplete(new WaveConfig()
+            {
+                BeforeWave = new Func<UniTask>[] 
+                {
+                    () => encounter.ItemsOperations.SpawnStartItems(new List<ItemData>() { tea, tea, tea ,tea })
+                },
+
+                Customers = new List<QueuedCustomer>
+                {
+                    new(bull) { OrderFactory = () => tea }
+                }
+            });
+        }
+
+        private async UniTask TheFirstWave()
+        {
             await encounter.CurrentWaveManager.DoWaveTillComplete(new WaveConfig()
             {
                 BeforeWave = new Func<UniTask>[]
@@ -62,7 +180,6 @@ namespace foxRestaurant
                 {
                     new(bull),
                     new(goat),
-                    new(hog),
                     new(cow),
                     new(hog),
                     new(cow),
@@ -72,6 +189,61 @@ namespace foxRestaurant
                     new(cow),
                     new(hog),
                     new(cow),
+                    new(hog),
+                }
+            });
+        }
+
+        private async UniTask TheSecondWave()
+        {
+            await encounter.CurrentWaveManager.DoWaveTillComplete(new WaveConfig()
+            {
+                BeforeWave = new Func<UniTask>[]
+                {
+                    () => encounter.ItemsOperations.SpawnStartItems()
+                },
+
+                Customers = new List<QueuedCustomer>
+                {
+                    new(bull),
+                    new(goat),
+                    new(cow),
+                    new(goat),
+                    new(cow),
+                    new(hog),
+                    new(goat),
+                    new(hog),
+                    new(cow),
+                    new(hog),
+                    new(cow),
+                    new(hog),
+                }
+            });
+        }
+
+        private async UniTask TheThirdWave()
+        {
+            await encounter.CurrentWaveManager.DoWaveTillComplete(new WaveConfig()
+            {
+                BeforeWave = new Func<UniTask>[]
+                {
+                    () => encounter.ItemsOperations.SpawnStartItems()
+                },
+
+                Customers = new List<QueuedCustomer>
+                {
+                    new(bull),
+                    new(goat),
+                    new(cow),
+                    new(goat),
+                    new(cow),
+                    new(hog),
+                    new(goat),
+                    new(goat),
+                    new(cow),
+                    new(hog),
+                    new(cow),
+                    new(hog),
                 }
             });
         }
