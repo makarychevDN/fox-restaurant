c43c2830d5df50831cccce177fbc2bb1865e6f96
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2026-07-29 13:35:29 +0500

    the 9th part of the 2nd level is localized

--- Assets/Game/Scripts/Scenarios/RiversideScenarioPart9.cs
+++ Assets/Game/Scripts/Scenarios/RiversideScenarioPart9.cs
@@ -19,45 +19,45 @@ namespace foxRestaurant
 
         protected override async UniTask StartScenarioTyped(ListenDialoguesEncounter encounter)
         {
-            await adele.Say("Я должна признать, вы отлично справились, лисятки.");
-            await adele.Say("Жителям Риверсайда стало получше и поспокойней.");
-            await adele.Say("Наконец-то закончится охота на ведьм.");
+            await adele.Say("I have to admit, you two did an excellent job, little foxes.".Locailze());
+            await adele.Say("The people of Riverside are feeling much better now.".Locailze());
+            await adele.Say("And maybe now this witch hunt will finally come to an end.".Locailze());
 
-            await red.Say("Так вы все-таки настоящая ведьма!");
+            await red.Say("So you really are a witch after all!".Locailze());
 
-            await adele.Say("Я настолько же ведьма, насколько ты леший.");
-            await adele.Say("Я гадалка, причем паршивая.<pause:0.5> Впрочем, как и все гадалки.");
-            await adele.Say("Хотя, для местных что в лоб, что по лбу.");
-            await adele.Say("Они с радостью поверят в самый невероятный вариант.");
-            await adele.Say("...");
-            await adele.Say("Ты меня отвлек.");
-            await adele.Say("Вы хорошо поработали и заслужили награду.");
-            await adele.Say("У меня и здесь и в Клиффорде есть какое-никакое влияние.");
-            await adele.Say("А еще у меня есть много занятных вещиц.");
-            await adele.Say("Каждому из вас дается по желанию.");
-            await adele.Say("Используйте их с умом, но не наглейте.");
+            await adele.Say("I'm about as much of a witch as you are a Leshy.".Locailze());
+            await adele.Say("I'm a fortune teller,<pause:0.5> and a terrible one at that.<pause:0.5> Then again, so are all fortune tellers.".Locailze());
+            await adele.Say("But to the locals, it's all the same.".Locailze());
+            await adele.Say("They'll gladly believe the most unbelievable explanation.".Locailze());
+            await adele.Say("...".Locailze());
+            await adele.Say("You distracted me.".Locailze());
+            await adele.Say("You've both worked hard, and you've earned yourselves a reward.".Locailze());
+            await adele.Say("I have a bit of influence both here and in Clifford.".Locailze());
+            await adele.Say("And I also happen to own quite a few interesting little things.".Locailze());
+            await adele.Say("Each of you gets one wish.".Locailze());
+            await adele.Say("Choose wisely,<pause:0.5> but don't get greedy.".Locailze());
 
-            await red.Say("...");
-            await red.Say("Я хочу себе оставить ту печь.");
+            await red.Say("...".Locailze());
+            await red.Say("I want to keep that oven.".Locailze());
 
-            await adele.Say("А у тебя губа не дура, Рыжий.");
-            await adele.Say("Хотя, цена справедливая, учитывая, что ты сделал.");
-            await adele.Say("Да и все равно она тут пылится без дела.");
-            await adele.Say("Терпеть не могу готовку.");
-            await adele.Say("Она твоя.");
-            await adele.Say("А ты чего хочешь, Серый?");
+            await adele.Say("You don't ask for small favors, do you, Red?".Locailze());
+            await adele.Say("But it's a fair price, considering what you've done.".Locailze());
+            await adele.Say("Besides, it's been gathering dust here anyway.".Locailze());
+            await adele.Say("I hate cooking.".Locailze());
+            await adele.Say("It's yours.".Locailze());
+            await adele.Say("And what about you, Silver?<pause:0.5> What do you want?".Locailze());
 
-            await silver.Say("Я хочу, чтобы вы нас доставили в Клиффорд как можно скорее.");
+            await silver.Say("I want you to get us to Clifford as soon as possible.".Locailze());
 
-            await adele.Say("<volume:0>.<pause:0.5>.<pause:0.5>.<pause:0.5><volume:1> Ладно");
-            await adele.Say("У нас для план Б для крайних случаев.");
-            await adele.Say("Думаю,<pause:0.5> можно им сегодня воспользоваться.");
-            await adele.Say("Но, если она мне нажалуется, что вы ее обижали...");
+            await adele.Say("t<volume:0>.<pause:0.5>.<pause:0.5>.<pause:0.5><volume:1> Fine.".Locailze());
+            await adele.Say("We do have a backup plan for emergencies.".Locailze());
+            await adele.Say("I suppose<pause:0.5> today qualifies.".Locailze());
+            await adele.Say("But if she comes back complaining that you two gave her a hard time...".Locailze());
 
-            await silver.Say("Мы будем джентельменами!");
+            await silver.Say("We'll be perfect gentlemen!".Locailze());
 
-            await adele.Say("Правильный ответ.");
-            await adele.Say("Собирайте вещи,<pause:0.5> ближайший рейс до Клиффорда через 10 минут.");
+            await adele.Say("That's the right answer.".Locailze());
+            await adele.Say("Pack your things,<pause:0.5> the next ride to Clifford leaves in ten minutes.".Locailze());
         }
     }
 }
