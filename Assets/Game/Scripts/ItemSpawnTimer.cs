48774a046fa97b24e00a7fa31deaf06519ec75bb
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2026-08-06 16:22:23 +0500

    skip time till next item spawn button has been added

--- Assets/Game/Scripts/ItemSpawnTimer.cs
+++ Assets/Game/Scripts/ItemSpawnTimer.cs
@@ -2,12 +2,14 @@ using DG.Tweening;
 using System.Linq;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace foxRestaurant
 {
     public class ItemSpawnTimer : MonoBehaviour, ITickable
     {
         [SerializeField] private ItemsSpawner spawner;
+        [SerializeField] private Button spawnButton;
         [SerializeField] private float spawnInterval = 5f;
         [SerializeField] private AudioSource errorSound;
         [SerializeField] private Transform tranformToShake;
@@ -23,6 +25,7 @@ namespace foxRestaurant
             this.restaurantEncounter = restaurantEncounter;
             restaurantEncounter.Ticker.AddTickable(this);
             timerDisplayer = restaurantEncounter.SlotsManager.SpawnerSlots.Last().GetComponentInChildren<TMP_Text>();
+            spawnButton.onClick.AddListener(SkipTimeTillSpawnItem);
             Pause();
         }
 
@@ -35,6 +38,8 @@ namespace foxRestaurant
                 return;
 
             timer += deltaTime;
+            timer = Mathf.Clamp(timer, 0f, spawnInterval);
+
             if (timer >= spawnInterval)
             {
                 if (restaurantEncounter.SlotsManager.SpawnerSlots.Count(slot => slot.Empty) == 0)
@@ -57,6 +62,17 @@ namespace foxRestaurant
 
             timerDisplayer.text = (spawnInterval - timer).ToString("<mspace=1em>0.0s</mspace>").Replace(',', ':');
             timerDisplayer.color = Extensions.HexToColor("#848f2e");
+        }
+
+        public void SkipTimeTillSpawnItem()
+        {
+            if (blocked || paused)
+                return;
+
+            float skippedTime = spawnInterval - timer;
+            restaurantEncounter.Ticker.TickAllTickables(skippedTime);
+            string skippedTimeString = skippedTime.ToString("0.0s");
+            restaurantEncounter.DynamicTextManager.SpawnDynamicText(timerDisplayer.transform.position + Vector3.left * 3, $"{skippedTimeString}\nspent", ReservedColors.YellowUI, timerDisplayer.transform.position + new Vector3(-3, 3, 0));
         }
 
         public void Pause() => paused = true;
