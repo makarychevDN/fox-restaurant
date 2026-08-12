f641d75258343f85d159382a1cf782afabbeba02
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2026-08-12 14:13:40 +0500

    dynamic difficulty descriptions have been added

--- Assets/Game/Scripts/Main Menu/DifficultySetupPanel.cs
+++ Assets/Game/Scripts/Main Menu/DifficultySetupPanel.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Components;
 using UnityEngine.UI;
 
 namespace foxRestaurant
@@ -13,6 +15,11 @@ namespace foxRestaurant
         [SerializeField] private Toggle easyToggle;
         [SerializeField] private Toggle normalToggle;
         [SerializeField] private Toggle hardToggle;
+        [SerializeField] private LocalizedString storyModeDifficultyDescription;
+        [SerializeField] private LocalizedString easyDifficultyDescription;
+        [SerializeField] private LocalizedString normalDifficultyDescription;
+        [SerializeField] private LocalizedString hardDifficultyDescription;
+        [SerializeField] private LocalizeStringEvent difficultyDescriptionEvent;
 
         private void Awake()
         {
@@ -29,7 +36,7 @@ namespace foxRestaurant
             if (difficulty == Difficulty.None)
             {
                 difficulty = Difficulty.Easy;
-                SetDifficulty(Difficulty.Easy);
+                SetDifficulty(Difficulty.Easy, easyDifficultyDescription);
             }
 
             UpdateTogglesVisualization(difficulty);
@@ -40,7 +47,7 @@ namespace foxRestaurant
             if (!isOn)
                 return;
 
-            SetDifficulty(Difficulty.StoryMode);
+            SetDifficulty(Difficulty.StoryMode, storyModeDifficultyDescription);
         }
 
         private void OnEasyChanged(bool isOn)
@@ -48,7 +55,7 @@ namespace foxRestaurant
             if (!isOn)
                 return;
 
-            SetDifficulty(Difficulty.Easy);
+            SetDifficulty(Difficulty.Easy, easyDifficultyDescription);
         }
 
         private void OnNormalChanged(bool isOn)
@@ -56,7 +63,7 @@ namespace foxRestaurant
             if (!isOn)
                 return;
 
-            SetDifficulty(Difficulty.Normal);
+            SetDifficulty(Difficulty.Normal, normalDifficultyDescription);
         }
 
         private void OnHardChanged(bool isOn)
@@ -64,13 +71,14 @@ namespace foxRestaurant
             if (!isOn)
                 return;
 
-            SetDifficulty(Difficulty.Hard);
+            SetDifficulty(Difficulty.Hard, hardDifficultyDescription);
         }
 
-        private void SetDifficulty(Difficulty difficulty)
+        private void SetDifficulty(Difficulty difficulty, LocalizedString difficultyDescription)
         {
             GameSettings.Difficulty = difficulty;
             PlayerPrefs.SetInt("Difficulty", (int)GameSettings.Difficulty);
+            difficultyDescriptionEvent.StringReference = difficultyDescription;
         }
 
         private void UpdateTogglesVisualization(Difficulty difficulty)
@@ -79,6 +87,22 @@ namespace foxRestaurant
             easyToggle.SetIsOnWithoutNotify(difficulty == Difficulty.Easy);
             normalToggle.SetIsOnWithoutNotify(difficulty == Difficulty.Normal);
             hardToggle.SetIsOnWithoutNotify(difficulty == Difficulty.Hard);
+
+            switch (GameSettings.Difficulty)
+            {
+                case Difficulty.StoryMode:
+                    difficultyDescriptionEvent.StringReference = storyModeDifficultyDescription;
+                    break;
+                case Difficulty.Easy:
+                    difficultyDescriptionEvent.StringReference = easyDifficultyDescription;
+                    break;
+                case Difficulty.Normal:
+                    difficultyDescriptionEvent.StringReference = normalDifficultyDescription;
+                    break;
+                case Difficulty.Hard:
+                    difficultyDescriptionEvent.StringReference = hardDifficultyDescription;
+                    break;
+            }
         }
     }
 }
